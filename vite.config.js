350cac7c6f509f351b7690f10d964cdfcf8b6725
Rifqi <rifqism88@gmail.com>
2025-12-10 11:10:55 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://suitmedia-backend.suitdev.com',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/storage': {
+        target: 'https://assets.suitdev.com/storage',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/storage/, ''),
+      },
+    },
+  },
+})
